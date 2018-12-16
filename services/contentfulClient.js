072aeff7307c8ba2660a49ba1daab1af93fc49a6
Allen Tan <tealtan@gmail.com>
2018-12-16 15:21:44 -0500

    Try removing config

--- services/contentfulClient.js
+++ services/contentfulClient.js
@@ -2,9 +2,8 @@ var contentful = require('contentful')
 var config = require('../package.json').config || {}
 
 var client = contentful.createClient({
-  accessToken: config.accessToken,
-  space: config.space
+  accessToken: '44f2b286e394198df930d60481ade424c2fead723f88767b154e1ce4ef560bcf',
+  space: '8730t1z03k68'
 })
 
 exports.client = client
-
