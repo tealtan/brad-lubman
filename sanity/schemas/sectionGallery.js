3945cd493465bf364341e6d830c052d3cf770b0a
Allen Tan <allen.tan@propublica.org>
2022-01-07 15:55:57 -0500

    Limit actions in section gallery schema

--- sanity/schemas/sectionGallery.js
+++ sanity/schemas/sectionGallery.js
@@ -2,7 +2,7 @@ export default {
   name: 'sectionGallery',
   title: 'Section: Gallery',
   type: 'document',
-  __experimental_actions: ['create', 'update', 'delete', 'publish'],
+  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
   fields: [
     {
       name: 'title',
